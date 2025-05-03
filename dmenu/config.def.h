b6be8a39fac944b8deb4f80d336391f55eb66a31
chicken-transfer4 <l.j.lee896@gmail.com>
2025-05-03 22:35:46 +0000

    Add files via upload

---
+++ dmenu/config.def.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	        /*     fg         bg       */
+	[SchemeNorm] = { "#A6E3A1", "#1E1E2E" },
+	[SchemeSel] = { "#CBA6F7", "#11111B" },
+	[SchemeSelHighlight] = { "#F38BA8", "#11111B" },
+	[SchemeNormHighlight] = { "#FAB387", "#1E1E2E" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 20;
+static unsigned int columns    = 3;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 3;
